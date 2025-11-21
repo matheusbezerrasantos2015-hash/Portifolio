c0e9d3a763bbd0d97f51b4afc1a112d9ef69ce5d
Matheus Bezerra <matheusbezerrasantos2015@gmail.com>
2025-11-21 15:47:52 -0300

    Adicionando Form PHP

--- forms/contact.php
+++ forms/contact.php
@@ -1,42 +1,39 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+// RECEIVING EMAIL
+$receiving_email_address = 'matheusbezerra7gbs@gmail.com';
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'matheusbezerra7gbs@gmail.com';
+if (file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php')) {
+    require($php_email_form);
+} else {
+    die('Unable to load PHP Email Form Library!');
+}
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+$contact = new PHP_Email_Form;
+$contact->ajax = true;
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+// SMTP CONFIG - GMAIL
+$contact->smtp = array(
+'host' => 'smtp.gmail.com',
+'username' => 'matheusbezerra7gbs@gmail.com',
+'password' => 'bylnuxpteqowzxlj', // coloque a senha de app do Gmail
+'port' => '587',
+'secure' => 'tls'
+);
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+// FORM DATA
+$contact->to = $receiving_email_address;
+$contact->from_name = $_POST['name'];
+$contact->from_email = $_POST['email'];
+$contact->subject = "Novo contato do site";
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  isset($_POST['phone']) && $contact->add_message($_POST['phone'], 'Phone');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+// EMAIL BODY
+$contact->add_message($_POST['name'], 'Nome');
+$contact->add_message($_POST['email'], 'Email');
+if (!empty($_POST['phone'])) {
+    $contact->add_message($_POST['phone'], 'Telefone');
+}
+$contact->add_message($_POST['message'], 'Mensagem', 10);
 
-  echo $contact->send();
+// SEND
+echo $contact->send();
 ?>
